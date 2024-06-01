2c97a2d0b1042cd27c27d5701899dd7839e79e91
sea_dog <83915874+seadog-www@users.noreply.github.com>
2024-06-01 16:17:47 +0800

    修改脚本名称 (#12)
    * 上传token model精简脚本
    
    * 修改脚本名称

--- run_clm.sh
+++ run_clm.sh
@@ -4,4 +4,4 @@ export PATH=$PATH:/home/yin/local_bin/miniconda3/envs/env_py310/bin
 rm -rf output/* cache_dir/*
 
 
-/home/yin/local_bin/miniconda3/envs/env_py310/bin/python3 run_clm.py run_config.json
+/home/yin/local_bin/miniconda3/envs/env_py310/bin/python3 clm_llama.py clm_config.json
